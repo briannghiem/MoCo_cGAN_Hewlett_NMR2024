7e08ec2ac01ab420477af8272f3d816fb2af802a
Brian <brian.nghiem@mail.utoronto.ca>
2025-03-19 18:42:12 -0400

    updated call to BatchNormalization

--- utils/generator_ops.py
+++ utils/generator_ops.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from tensorflow.python.keras.layers import Concatenate, Dropout, MaxPooling3D
 from tensorflow.python.keras.layers.convolutional import Conv3D, Conv3DTranspose, UpSampling3D
-from tensorflow.python.keras.layers.normalization import BatchNormalization
+# from tensorflow.python.keras.layers.normalization import BatchNormalization
+from tensorflow.keras.layers import BatchNormalization
 from tensorflow.python.keras.models import Input, Model
 
 class get_generator(object):
