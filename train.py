e00cd4b9c560e9ddcb00f4f6745668394dc2437f
Brian <brian.nghiem@mail.utoronto.ca>
2025-03-20 10:42:03 -0400

    update file paths

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from utils.dataset_prep import prepare_train_data
 from utils.model_config import Config
 
 # Options
-model_type = 'cGAN_singlechannel_complex' # 'cGAN_complex' (performs motion correction on complex coil-combined data)
+model_type = 'cGAN_complex' # 'cGAN_complex' (performs motion correction on complex coil-combined data)
                                           # 'cGAN_singlechannel_complex' (performes motion correction on each complex channel independantly)
                                           # 'cGAN_multichannel_complex' (performs motion correction on all complex channels simultaneously)
                                           # Each variation can also be trained to perform correction on magnitude data by removing '_complex'
@@ -17,9 +17,9 @@ if model_type[0:17] == 'cGAN_multichannel':
     config = Config(model_type,filters=[2048,1024,512,256])
 else:
     config = Config(model_type)
-config.load.opt = 1 # load saved models (turn off to train from scratch)
+config.load.opt = 0 # load saved models (turn off to train from scratch)
 
-config.data.dir = '\path\to\data'
+config.data.dir = r'/home/nghiemb/RMC_repos/MoCo_cGAN/data/training_dataset/slices'
 
 # Set up datasets
 train_data = prepare_train_data(config,'training')
