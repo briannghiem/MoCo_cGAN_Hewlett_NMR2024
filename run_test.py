fd1843a19a1a566ee9e498fd5608913f5359ca6f
briannghiem <brian.nghiem@mail.utoronto.ca>
2025-03-25 16:35:26 -0400

    updating data loading

--- run_test.py
+++ run_test.py
@@ -8,7 +8,7 @@ Retraining on T1w MPRAGE data from Calgary-Campinas dataset, with simulated moti
 
 #-------------------------------------------------------------------------------
 from utils.cgan_ops import cGAN
-from utils.dataset_prep import prepare_train_data
+from utils.dataset_prep import prepare_test_data
 from utils.model_config import Config
 
 import os
@@ -28,47 +28,35 @@ set_session(tf.compat.v1.Session(config=config))
 #-------------------------------------------------------------------------------
 # SETTING UP MODEL
 
-model_type = 'cGAN_complex' 
-'''
-'cGAN_complex' (performs motion correction on complex coil-combined data)
-'cGAN_singlechannel_complex' (performes motion correction on each complex channel independantly)
-'cGAN_multichannel_complex' (performs motion correction on all complex channels simultaneously)
-Each variation can also be trained to perform correction on magnitude data by removing '_complex'
-'''
+model_type = 'cGAN_complex'
 
-# Configure settings
-if model_type[0:17] == 'cGAN_multichannel':
-    config = Config(model_type,filters=[2048,1024,512,256])
-else:
-    config = Config(model_type)
+config = Config(model_type)
 
-config.load.opt = 0 # load saved models (turn off to train from scratch)
+#Loading pre-trained model
 config.data.dir = r'/home/nghiemb/Data/CC/simulated_datasets/MoCo_cGAN_Hewlett_NMR2024'
 
-'''
-if config.load.opt:
-    if model_type ==  'cGAN_complex':
-        config.load.checkpoint = '73'
-    elif model_type == 'cGAN_singlechannel_complex':
-        config.load.checkpoint = '64'
-    elif model_type == 'cGAN_multichannel_complex':
-        config.load.checkpoint = '47'  
-    config.training.num_epochs = int(config.load.checkpoint) - 1 # skip training and stick with saved model
-'''
+config.load.opt = 1 # load saved models
+config.load.checkpoint = '100'
+config.training.num_epochs = int(config.load.checkpoint) - 1 # skip training and stick with saved model
 
-# Configure model
 model = cGAN(config)
-# model.summary() #creates and saves model txt file in current working directory
 
 #-------------------------------------------------------------------------------
-# # LOADING DATA
+# # LOADING TEST DATA
+mpath = r'/home/nghiemb/RMC_repos/MoCo_cGAN_Hewlett_NMR2024'
+dpath = r'/home/nghiemb/Data/CC/simulated_datasets/MoCo_cGAN_Hewlett_NMR2024/data/testing' #
+
+paradigms = [r'/Paradigm_1C', r'/Paradigm_1D', r'/Paradigm_1E', r'/Paradigm_1F']
+subs = [1,4,5,6,7]
+
+paradigm = paradigms[0]
+sub = subs[0]
+dpath_temp_root = dpath + paradigm + r'/Test{}'.format(sub)
+
 
 # Set up datasets
-train_data = prepare_train_data(config,'training')
-validation_data = prepare_train_data(config,'validation')
+test_data = prepare_test_data(config, dpath_temp_root)
 
-# # TRAINING
-model.train(train_data=train_data, val_data=validation_data)
 
 # # Sample output
 # model.sample_output(validation_data,'Validation Example',config.training.num_epochs)
@@ -96,67 +84,5 @@ def plot_views(img, vmax = 1.0):
             ax.imshow(img[:,:,img.shape[2]//2], cmap = "gray", vmax = vmax)
         #
     plt.show()
-
-'''
-
-'''
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-
-#-------------------------------
-#Helper functions
-def get_ymax(train_array, val_array):
-    ymax_init = np.max((train_array[0], val_array[0]))
-    ymax = 10**(math.ceil(math.log(ymax_init, 10)))
-    return ymax
-
-def get_ymin(train_array, val_array):
-    ymin_init = np.min((train_array[-1], val_array[-1]))
-    ymin = 10**(math.floor(math.log(ymin_init, 10)))
-    return ymin
-
-def get_curves(train_array, val_array, y_axis_title, ylims = 'default'):
-    epoch_array = np.arange(1,len(train_array)+1)
-    if ylims == 'default':
-        ymin = np.min((min(train_array), min(val_array)))
-        ymax = np.max((max(train_array), max(val_array)))
-    elif ylims == 'cropped':
-        ymin = get_ymin(train_array, val_array)
-        ymax = get_ymax(train_array, val_array)
-    else:
-        ymin = ylims[0]
-        ymax = ylims[1]
-    #
-    plt.figure()
-    plt.plot(epoch_array, train_array, label = "training")
-    plt.plot(epoch_array, val_array, label = "validation")
-    plt.xlabel("Epoch")
-    plt.ylabel(y_axis_title)
-    plt.title("cGAN - Loss Curves; {}".format(y_axis_title))
-    plt.ylim(ymin, ymax)
-    plt.show()
-
-#-------------------------------
-mpath = r'/home/nghiemb/RMC_repos/MoCo_cGAN_Hewlett_NMR2024'
-spath = mpath + r'/savedModels/cGAN_complex/checkpoints'
-loss = np.load(spath + r'/loss.npy', allow_pickle=1).item() #recover dictionary from 0-array
-
-keys = list(loss.keys())
-
-#-------------------------------
-#Mean Absolute Error
-MAE_train = loss[keys[-2]]['training']
-MAE_val = loss[keys[-2]]['validation']
-
-get_curves(MAE_train, MAE_val, 'MAE', ylims = 'default')
-
-#Binary Cross Entropy
-cGAN_CE_train = loss[keys[-1]]['training'] #cross entropy
-cGAN_CE_val = loss[keys[-1]]['validation'] #cross entropy
-
-get_curves(cGAN_CE_train, cGAN_CE_val, 'Cross Entropy', ylims = 'default')
-get_curves(np.log(cGAN_CE_train), np.log(cGAN_CE_val), 'Cross Entropy - Log_e plot', ylims = 'default')
-get_curves(np.log(cGAN_CE_train)/np.log(10), np.log(cGAN_CE_val)/np.log(10), 'Cross Entropy - Log_10 plot', ylims = 'default')
 
 '''
